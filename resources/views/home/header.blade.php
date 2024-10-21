8a72ff3f41a325250786b8031bc969ecbe03cfdc
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-10-21 10:59:41 +0100

    feat(ui):Implemented new template

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,59 +1,153 @@
-<header class="header_section">
-            <div class="container">
-               <nav class="navbar navbar-expand-lg custom_nav-container ">
-                  <a class="navbar-brand" href="index.html"><img width="250" src="images/logo.png" alt="#" /></a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class=""> </span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                     <ul class="navbar-nav">
-                        <li class="nav-item active">
-                           <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                       <li class="nav-item dropdown">
-                           <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Pages <span class="caret"></span></a>
-                           <ul class="dropdown-menu">
-                              <li><a href="about.html">About</a></li>
-                              <li><a href="testimonial.html">Testimonial</a></li>
-                           </ul>
-                        </li>
-                        <li class="nav-item">
-                           <a class="nav-link" href="product.html">Products</a>
-                        </li>
-                        <li class="nav-item">
-                           <a class="nav-link" href="blog_list.html">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                           <a class="nav-link" href="contact.html">Contact</a>
-                        </li>
-                        @if (Route::has('login'))
+<header class="header_area">
+    <div class="top_menu">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7">
+            <div class="float-left">
+              <p>Phone: +01 256 25 235</p>
+              <p>email: info@eiser.com</p>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <div class="float-right">
+              <ul class="right_side">
+                <li>
+                  <a href="cart.html">
+                    gift card
+                  </a>
+                </li>
+                <li>
+                  <a href="tracking.html">
+                    track order
+                  </a>
+                </li>
+                <li>
+                  <a href="contact.html">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main_menu">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light w-100">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <a class="navbar-brand logo_h" href="index.html">
+            <img src="img/logo.png" alt="" />
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse offset w-100" id="navbarSupportedContent">
+            <div class="row w-100 mr-0">
+              <div class="col-lg-7 pr-0">
+                <ul class="nav navbar-nav center_nav pull-right d-flex align-items-center">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home</a>
+                  </li>
+                  <li class="nav-item submenu dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                      aria-expanded="false">Shop</a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a class="nav-link" href="category.html">Shop Category</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="single-product.html">Product Details</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="checkout.html">Product Checkout</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="cart.html">Shopping Cart</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item submenu dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                      aria-expanded="false">Blog</a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a class="nav-link" href="blog.html">Blog</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="single-blog.html">Blog Details</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item submenu dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                      aria-expanded="false">Pages</a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a class="nav-link" href="tracking.html">Tracking</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="elements.html">Elements</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contact.html">Contact</a>
+                  </li>
+                  </li>
+     
+
+
+                </ul>
+              </div>
+
+              <div class="col-lg-5 pr-0">
+                <ul class="nav navbar-nav navbar-right right_nav pull-right align-items-center">
+                  <li class="nav-item">
+                    <a href="#" class="icons">
+                      <i class="ti-search" aria-hidden="true"></i>
+                    </a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a href="#" class="icons">
+                      <i class="ti-shopping-cart"></i>
+                    </a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a href="#" class="icons">
+                      <i class="ti-user" aria-hidden="true"></i>
+                    </a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a href="#" class="icons">
+                      <i class="ti-heart" aria-hidden="true"></i>
+                    </a>
+                  </li>
+                
+                  @if (Route::has('login'))
                         @auth
                         <li class="nav-item">
                             <x-app-layout>
                             </x-app-layout>
                         </li>
-
-
                         @else
-                        <li class="nav-item">
-                           <a class="btn btn-primary" href="{{route('login')}}">Login</a>
+                        <li class="">
+                           <a class="btn btn-outline-success" href="{{route('login')}}">Login</a>
                         </li>
                          @endauth
                         @endif
-        
-                        <li class="nav-item">
-                           <a class="nav-link" href="#">
-                           <i class="fa fa-shopping-cart"></i>
-
-                           </a>
-                        </li>
-                        <form class="form-inline">
-                           <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                           <i class="fa fa-search" aria-hidden="true"></i>
-                           </button>
-                        </form>
-                     </ul>
-                  </div>
-               </nav>
+                </ul>
+              </div>
             </div>
-         </header>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -1,72 +1,85 @@
 <!DOCTYPE html>
-<html>
-   <head>
-      <!-- Basic -->
-      <meta charset="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <!-- Mobile Metas -->
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <!-- Site Metas -->
-      <meta name="keywords" content="" />
-      <meta name="description" content="" />
-      <meta name="author" content="" />
-      <link rel="shortcut icon" href="home/images/favicon.png" type="">
-      <title>Famms - Fashion HTML Template</title>
-      <!-- bootstrap core css -->
-      <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
-      <!-- font awesome style -->
-      <link href="home/css/font-awesome.min.css" rel="stylesheet" />
-      <!-- Custom styles for this template -->
-      <link href="home/css/style.css" rel="stylesheet" />
-      <!-- responsive style -->
-      <link href="home/css/responsive.css" rel="stylesheet" />
-   </head>
-   <body>
-      <div class="hero_area">
-         <!-- header section strats -->
-   @include('home.header')
-         <!-- end header section -->
-         <!-- slider section -->
-   @include('home.slider')
-         <!-- end slider section -->
-      </div>
-      <!-- why section -->
-   @include('home.why')
-      <!-- end why section -->
-      
-      <!-- arrival section -->
-   @include('home.arrival')
-      <!-- end arrival section -->
-      
-      <!-- product section -->
-   @include('home.product')
-      <!-- end product section -->
+<html lang="en">
 
-      <!-- subscribe section -->
-   @include('home.subscribe')
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="icon" href="home/img/favicon.png" type="image/png" />
+  <title>gameRT ecommerce</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="home/css/bootstrap.css" />
+  <link rel="stylesheet" href="home/vendors/linericon/style.css" />
+  <link rel="stylesheet" href="home/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="home/css/themify-icons.css" />
+  <link rel="stylesheet" href="home/css/flaticon.css" />
+  <link rel="stylesheet" href="home/vendors/owl-carousel/owl.carousel.min.css" />
+  <link rel="stylesheet" href="home/vendors/lightbox/simpleLightbox.css" />
+  <link rel="stylesheet" href="home/vendors/nice-select/css/nice-select.css" />
+  <link rel="stylesheet" href="home/vendors/animate-css/animate.css" />
+  <link rel="stylesheet" href="home/vendors/jquery-ui/jquery-ui.css" />
+  <!-- main css -->
+  <link rel="stylesheet" href="home/css/style.css" />
+  <link rel="stylesheet" href="home/css/responsive.css" />
+</head>
 
-      <!-- end subscribe section -->
-      <!-- client section -->
-   @include('home.client')
+<body>
+  <!--================Header Menu Area =================-->
+ @include('home.header')
+  <!--================Header Menu Area =================-->
+  <!--================Home Banner Area =================-->
+  @include('home.banner')
+  <!--================End Home Banner Area =================--> 
+  <!-- Start feature Area -->
+  @include('home.feature')
 
-      <!-- end client section -->
-      <!-- footer start -->
-   @include('home.footer')
-      <!-- footer end -->
-      <div class="cpy_">
-         <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
-         
-            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
-         
-         </p>
-      </div>
-      <!-- jQery -->
-      <script src="home/js/jquery-3.4.1.min.js"></script>
-      <!-- popper js -->
-      <script src="home/js/popper.min.js"></script>
-      <!-- bootstrap js -->
-      <script src="home/js/bootstrap.js"></script>
-      <!-- custom js -->
-      <script src="home/js/custom.js"></script>
-   </body>
+  <!-- End feature Area -->
+
+  <!--================ Feature Product Area =================-->
+  @include('home.productFeature')
+  <!--================ End Feature Product Area =================-->
+
+  <!--================ Offer Area =================-->
+  @include('home.offer')
+
+  <!--================ End Offer Area =================-->
+
+  <!--================ New Product Area =================-->
+  @include('home.productNew')
+
+  <!--================ End New Product Area =================-->
+
+  <!--================ Inspired Product Area =================-->
+  @include('home.productInspired')
+
+  <!--================ End Inspired Product Area =================-->
+
+  <!--================ Start Blog Area =================-->
+  @include('home.blog')
+
+  <!--================ End Blog Area =================-->
+
+  <!--================ start footer Area  =================-->
+  @include('home.footer')
+
+  <!--================ End footer Area  =================-->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/stellar.js"></script>
+  <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+  <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+  <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+  <script src="vendors/isotope/isotope-min.js"></script>
+  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+  <script src="vendors/counter-up/jquery.counterup.js"></script>
+  <script src="js/mail-script.js"></script>
+  <script src="js/theme.js"></script>
+</body>
+
 </html>
