bdeb31c524ea0dc4d11c935431e053ef9c0fd4fd
taoufikbnr <69259869+taoufikbnr@users.noreply.github.com>
2024-10-23 14:45:46 +0100

    change logo login/register

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,6 +1,6 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
-        <img src="https://st4.depositphotos.com/5040187/19683/v/450/depositphotos_196837274-stock-illustration-logo-swoosh-ellipse-green-letter.jpg" alt="" style="width:200px">
+        <img src="{{asset('/img/logo.png')}}" alt="" style="width:200px">
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
